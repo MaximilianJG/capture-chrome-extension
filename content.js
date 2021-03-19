776982ae06f4d575439ec373345e9ade49164236
Ben Smith <benhsmith13@gmail.com>
2021-03-19 14:23:33 -0600

    inserts button on highlight

--- content.js
+++ content.js
@@ -1,13 +1,30 @@
-addEventListener('mouseup', e => {
-  if (!window.getSelection) { return; }
+const highlignts = {};
+
+const captureButton = makeCaptureButton();
+
+document.addEventListener('mouseup', e => {
+  if (!window.getSelection) { return; } // return if browser doesn't support selection for some reason
   const selection = window.getSelection();
-  const selectionText = selection.toString();
-  if (selectionText) {
-    const range = selection.getRangeAt(0);
-    range.deleteContents();
+  const range = selection.getRangeAt(0);
+  if (e.target && e.target.id === 'captureButton') { // highlight button clicked
+    document.getElementById('captureButton').remove();
+    const selectionText = selection.toString();
     const highlignt = document.createElement('span');
     highlignt.style.backgroundColor = 'yellow';
-    highlignt.textContent = selectionText;
-    range.insertNode(highlignt);
+    // highlignt.textContent = selectionText;
+    highlignt.style.position = 'relative';
+    range.surroundContents(highlignt);
+  } else if (selection.toString()) { // text highlighted 
+    if (selection.anchorNode.nodeType === 3) { // is a text node
+      const contents = range.extractContents();
+      console.log(contents.toString());
+      range.deleteContents();
+      range.insertNode(captureButton);
+      range.insertNode(contents);
+    } else {
+      console.log(selection.anchorNode.nodeType, selection.anchorNode.nodeName);
+    }
+  } else { // any other click on the page
+    removeCaptureButtonFromDOM();
   }
 });
---
+++ uiElements/captureButton.js
@@ -0,0 +1,20 @@
+function makeCaptureButton() {
+  const captureButton = document.createElement('button');
+  captureButton.id = 'captureButton';
+  captureButton.innerHTML = `<img src='${chrome.runtime.getURL('assets/capture16.png')}' alt='capture' />`;
+  captureButton.style.position = 'absolute';
+  captureButton.style.backgroundColor = '#FFFFFF';
+  captureButton.style.border = '0.5px solid #D8D7D5';
+  captureButton.style.borderRadius = '100%';
+  captureButton.style.boxSizing = 'border-box';
+  captureButton.style.boxShadow = '0px 4px 4px rgba(0, 0, 0, 0.25)';
+
+  return captureButton;
+}
+
+function removeCaptureButtonFromDOM() {
+  const button = document.getElementById('captureButton');
+    if (button) {
+      button.remove();
+    }
+}
