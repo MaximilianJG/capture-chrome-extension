bc885c2c79b251ca7272191f0dc4961ce6cdaa9b
Ben Smith <benhsmith13@gmail.com>
2021-04-20 11:47:01 -0600

    fixes delete button and send button

--- uiElements/commentPopup.js
+++ uiElements/commentPopup.js
@@ -10,7 +10,7 @@ function makeCommentPopup(id, cb) {
     popup.id = `capture-comment-popup-${id}`;
     popup.className = 'capture-comment-popup';
     const form = popup.getElementsByTagName('form')[0];
-    const trashButton = popup.getElementsByTagName('button')[1];
+    const trashButton = popup.getElementsByTagName('button')[0];
   
     trashButton.onclick = e => {
       e.stopPropagation();
@@ -21,7 +21,6 @@ function makeCommentPopup(id, cb) {
   
     form.onsubmit = e => {
       e.preventDefault();
-      console.log('submitting');
       const comment = form.getElementsByTagName('input')[0].value;
       postComment({
         quoteId: id,
@@ -37,5 +36,8 @@ function makeCommentPopup(id, cb) {
 
 function showCommentPopup(id) {
   const popup = document.getElementById(`capture-comment-popup-${id}`);
-  popup.style.display = 'block';
+  const isDisabled = document.getElementById(`capture-highlight-${id}`).classList.contains('deleted');
+  if (!isDisabled) {
+    popup.style.display = 'block';
+  }
 }
